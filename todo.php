f01fab2b820a02662f863d26ea0a21a0ac180de7
iva bigpines <iva@bigpines.com>
2023-07-29 16:14:55 +1000

    update remove ventor

---
+++ todo.php
@@ -0,0 +1,15 @@
+<?php
+
+/* my unofficial todo list
+
+Connect to colab
+- on user signup asign a random app{rand}.ipynb
+- get {rand} from db and update db as used by user_id
+- on user login get {rand} from db and add to $_SESSION['colab'] = {rand}
+
+Colab connected indicator
+- use JavaScript to ping ngrok url every minute for status
+- if status is 200 then show connected via green icon
+- if status is 404 then show disconnected via red icon with button to "Open in Colab" link
+
+*/
