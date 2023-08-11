771cf6a62e32e7754b682ee464db6c675d0e060b
iva bigpines <iva@bigpines.com>
2023-08-11 14:46:34 +1000

    apitest

---
+++ apitest.php
@@ -0,0 +1,34 @@
+<?php
+include 'include.php';
+$con = new mysqli($sqlh, $sqlu, $sqlp, $sqld);
+
+if ($con->connect_errno) {
+    printf("connection failed: %s\n", $con->connect_error());
+    exit();
+}
+
+
+// Check if the POST request is not empty
+if (!empty($_POST)) {
+
+    // Capture all $_POST data
+    $logData = json_encode($_POST);
+
+    // Prepare a SQL statement to insert the data into the apitest table
+    $stmt = $con->prepare("INSERT INTO apitest (log, created_at) VALUES (?, NOW())");
+
+    // Bind the log data to the prepared statement
+    $stmt->bind_param('s', $logData);
+
+    // Execute the statement
+    if ($stmt->execute()) {
+        echo "Data logged successfully.";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    // Close the statement
+    $stmt->close();
+} else {
+    echo "No POST data to log.";
+}
