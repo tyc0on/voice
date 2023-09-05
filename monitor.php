f1921f2da8da0daf46177bc9d58a4802372882f8
iva bigpines <iva@bigpines.com>
2023-09-05 19:27:32 +1000

    mon

---
+++ monitor.php
@@ -0,0 +1,10 @@
+<?php
+$filename = basename(__FILE__, '.php'); // Get the filename without the extension
+$coreFilePath = 'core/' . $filename . '.php'; // Build the path to the core file
+
+if (file_exists($coreFilePath)) {
+    include $coreFilePath;
+} else {
+    echo "Core file not found: $coreFilePath";
+}
+?>
