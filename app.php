9fa05c1231c17564f36ebd4faae847f88b776dce
iva bigpines <iva@bigpines.com>
2023-09-05 20:25:28 +1000

    login logic

--- app.php
+++ app.php
@@ -26,9 +26,11 @@ if (!isset($_SESSION['loggedin'])) {
 	$_SESSION['return_url'] = $_SERVER['REQUEST_URI'];
 	// header('Location: ' . $rdir);
 
-	// Check for the remember token
 	if (isset($_COOKIE['remember_token'])) {
 		$remember_token = $_COOKIE['remember_token'];
+
+		// First, check the old remember_token column in the accounts table
+		$userFound = false;
 		if ($stmt = $con->prepare('SELECT id, accounttype, username, email, fullname, picture FROM accounts WHERE remember_token = ?')) {
 			$stmt->bind_param('s', $remember_token);
 			$stmt->execute();
@@ -37,20 +39,37 @@ if (!isset($_SESSION['loggedin'])) {
 			if ($stmt->num_rows > 0) {
 				$stmt->bind_result($id, $accounttype, $setusername, $email, $name, $picture);
 				$stmt->fetch();
-				session_regenerate_id();
-				$_SESSION['loggedin'] = TRUE;
-				$_SESSION['name'] = $name;
-				$_SESSION['username'] = $setusername;
-				$_SESSION['email'] = $email;
-				$_SESSION['id'] = $id;
-				$_SESSION['accounttype'] = $accounttype;
-				$_SESSION['picture'] = $picture;
-				$loggedin = "true";
+				$userFound = true;
 			}
 			$stmt->close();
 		}
-	}
 
+		// If not found in the old method, check the new remember_tokens table
+		if (!$userFound && $stmt = $con->prepare('SELECT a.id, a.accounttype, a.username, a.email, a.fullname, a.picture FROM accounts a JOIN remember_tokens rt ON a.id = rt.user_id WHERE rt.token = ?')) {
+			$stmt->bind_param('s', $remember_token);
+			$stmt->execute();
+			$stmt->store_result();
+
+			if ($stmt->num_rows > 0) {
+				$stmt->bind_result($id, $accounttype, $setusername, $email, $name, $picture);
+				$stmt->fetch();
+				$userFound = true;
+			}
+			$stmt->close();
+		}
+
+		if ($userFound) {
+			session_regenerate_id();
+			$_SESSION['loggedin'] = TRUE;
+			$_SESSION['name'] = $name;
+			$_SESSION['username'] = $setusername;
+			$_SESSION['email'] = $email;
+			$_SESSION['id'] = $id;
+			$_SESSION['accounttype'] = $accounttype;
+			$_SESSION['picture'] = $picture;
+			$loggedin = "true";
+		}
+	}
 	// If the user is not logged in and the remember token doesn't exist, save the return URL
 	if (!$loggedin) {
 		$_SESSION['return_url'] = $_SERVER['REQUEST_URI'];
@@ -62,8 +81,13 @@ if (!isset($_SESSION['loggedin'])) {
 			$_SESSION['name'] = "Local User";
 			$_SESSION['id'] = 1;
 			$loggedin = "true";
+		} else {
+			// user not logged in, redirect so sign-in
+			header('Location: /sign-in');
+			exit;
 		}
 	}
+
 	// exit;
 } else {
 	$loggedin = "true";
