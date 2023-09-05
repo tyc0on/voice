f248bf3a95a7c7da804e8a7b53842b7ca384ae75
iva bigpines <iva@bigpines.com>
2023-09-05 20:18:45 +1000

    test

--- config.php
+++ config.php
@@ -83,26 +83,7 @@ EOD;
 
 
 
-$menu1 = <<<EOD
-<div class="app-header-menu app-header-mobile-drawer align-items-stretch" data-kt-drawer="true" data-kt-drawer-name="app-header-menu" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="250px" data-kt-drawer-direction="end" data-kt-drawer-toggle="#kt_app_header_menu_toggle" data-kt-swapper="true" data-kt-swapper-mode="{default: 'append', lg: 'prepend'}" data-kt-swapper-parent="{default: '#kt_app_body', lg: '#kt_app_header_wrapper'}">
-                            <!--begin::Menu-->
-                            <div class="menu menu-rounded menu-column menu-lg-row my-5 my-lg-0 align-items-stretch fw-semibold px-2 px-lg-0" id="kt_app_header_menu" data-kt-menu="true">
-                                <!-- <div data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="bottom-start" class="menu-item menu-lg-down-accordion menu-sub-lg-down-indention me-0 me-lg-2"><span class="menu-link"><span class="menu-title">Help</span><span class="menu-arrow d-lg-none"></span></span>
-                                    <div class="menu-sub menu-sub-lg-down-accordion menu-sub-lg-dropdown px-lg-2 py-lg-4 w-lg-200px" style="">
-                                        <div class="menu-item"><a class="menu-link" href="https://preview.keenthemes.com/html/metronic/docs/base/utilities" target="_blank" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss="click" data-bs-placement="right" data-bs-original-title="Check out over 200 in-house components, plugins and ready for use solutions" data-kt-initialized="1"><span class="menu-icon"><i class="ki-duotone ki-rocket fs-2"><span class="path1"></span><span class="path2"></span></i></span><span class="menu-title">Components</span></a></div>
-                                    </div>
-                                </div> -->
-                                <div class="menu-item">
-                                    <a href="https://discord.gg/GEzDY6cy" target="_blank" class="menu-link">
-                                        <span class="menu-icon">
-                                            <i class="bi bi-discord fs-3"></i> </span>
-                                        <span class="menu-title">Discord</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
 
-EOD;
 
 
 
@@ -217,4 +198,27 @@ $menu2 .= <<<EOD
 </div>
 <!--end::Menu wrapper-->
 </div>
+EOD;
+
+
+
+$menu1 = <<<EOD
+<div class="app-header-menu app-header-mobile-drawer align-items-stretch" data-kt-drawer="true" data-kt-drawer-name="app-header-menu" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="250px" data-kt-drawer-direction="end" data-kt-drawer-toggle="#kt_app_header_menu_toggle" data-kt-swapper="true" data-kt-swapper-mode="{default: 'append', lg: 'prepend'}" data-kt-swapper-parent="{default: '#kt_app_body', lg: '#kt_app_header_wrapper'}">
+                            <!--begin::Menu-->
+                            <div class="menu menu-rounded menu-column menu-lg-row my-5 my-lg-0 align-items-stretch fw-semibold px-2 px-lg-0" id="kt_app_header_menu" data-kt-menu="true">
+                                <!-- <div data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="bottom-start" class="menu-item menu-lg-down-accordion menu-sub-lg-down-indention me-0 me-lg-2"><span class="menu-link"><span class="menu-title">Help</span><span class="menu-arrow d-lg-none"></span></span>
+                                    <div class="menu-sub menu-sub-lg-down-accordion menu-sub-lg-dropdown px-lg-2 py-lg-4 w-lg-200px" style="">
+                                        <div class="menu-item"><a class="menu-link" href="https://preview.keenthemes.com/html/metronic/docs/base/utilities" target="_blank" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-dismiss="click" data-bs-placement="right" data-bs-original-title="Check out over 200 in-house components, plugins and ready for use solutions" data-kt-initialized="1"><span class="menu-icon"><i class="ki-duotone ki-rocket fs-2"><span class="path1"></span><span class="path2"></span></i></span><span class="menu-title">Components</span></a></div>
+                                    </div>
+                                </div> -->
+                                <div class="menu-item">
+                                    <a href="https://discord.gg/GEzDY6cy" target="_blank" class="menu-link">
+                                        <span class="menu-icon">
+                                            <i class="bi bi-discord fs-3"></i> </span>
+                                        <span class="menu-title">Discord</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
 EOD;
