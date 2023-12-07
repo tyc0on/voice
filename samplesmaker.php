96064416557a860feb49b8b5c252865e2ed59be8
Yupinnu <83113114+Yupinnu@users.noreply.github.com>
2023-12-07 12:56:58 +1000

    samplesmaker test

--- samplesmaker.php
+++ samplesmaker.php
@@ -9,12 +9,10 @@ if ($con->connect_errno) {
 
 $folder = "/var/www/easyaivoice.com/public_html/samples";
 $file = "sample.mp3";
-// create samples folder if not exists
 if (!file_exists($folder)) {
     mkdir($folder, 0755, true);
 }
 
-// loop through all `files` and create job for each with curl
 $sql = "SELECT * FROM files WHERE md5 <> 'T' AND index_name != '' ORDER BY `id` DESC";
 $stmt = $con->prepare($sql);
 $stmt->execute();
@@ -35,39 +33,26 @@ $channel->queue_declare('job_queue', false, true, false, false);
 $pitches = array(-16, -12, -8, -4, 0, 4, 8, 12, 16);
 
 while ($row = $result->fetch_assoc()) {
-    // url is the url of the audio file to process
     $url = "https://easyaivoice.com/" . $file;
-    // $name = $row['name'];
-    //model url
     $name = $row['url'];
 
-    // lookup files.name where files.url = $name
+    // Retrieve the model name from the database
     $sql = "SELECT * FROM files WHERE url = ?";
     $stmt = $con->prepare($sql);
     $stmt->bind_param('s', $name);
     $stmt->execute();
     $result2 = $stmt->get_result();
     $stmt->close();
-    // set model_name to files.name
     if ($result2->num_rows > 0) {
         $row2 = $result2->fetch_assoc();
         $model_name = $row2['name'];
-    } else {
-        //die("Model not found.");
-    }
+    } 
 
-
-
-    //if file samples/$name.p4.mp3 exists skip
-    $existing = $folder . "/" . $model_name . ".p4.mp3"; // __DIR__ . 
-    echo $existing . "<br>\n";
-    if (file_exists($existing)) {
-        // continue;
-    } else {
-
-
-
-        foreach ($pitches as $pitch) {
+    // Check for file existence for each pitch
+    foreach ($pitches as $pitch) {
+        $existing = $folder . "/" . $model_name . ".p" . $pitch . ".mp3";
+        echo $existing . "<br>\n";
+        if (!file_exists($existing)) {
 
             $jobData = array(
                 'audio_url' => $url,
@@ -101,7 +86,6 @@ while ($row = $result->fetch_assoc()) {
             $channel->basic_publish($msg, '', 'job_queue');
         }
     }
-    // die;
 }
 $channel->close();
 $connection->close();
