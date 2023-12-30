6fa1cbc760503a1e2472f4197865eb11a271b9d1
ivap <ivap@gmail.com>
2023-12-30 15:40:09 +1000

    error off

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 //errors on
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 ini_set('session.cookie_lifetime', 60 * 60 * 24 * 365);
 session_start();
